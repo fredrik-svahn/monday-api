3085f77177c03172756ff461bedb88199d5b7366
Fredrik Svahn <frsvahn@kth.se>
2019-03-07 11:08:39 +0100

    corrected url

--- monday.js
+++ monday.js
@@ -24,7 +24,7 @@ class Monday {
     addNewNote(pulseId,title,content) {
         return this.axios({
             method:'post',
-            url: `https://api.monday.com:443/v1/pulses/${pulseId}/notes.json`,
+            url: `/pulses/${pulseId}/notes.json`,
             data:
             {
                 "title" : title,
